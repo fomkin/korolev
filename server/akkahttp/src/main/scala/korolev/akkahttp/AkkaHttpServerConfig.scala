b08541f21a874942d7b32c8b25846de12f8f6195
Alexander Semenov <tvaroh@icloud.com>
2018-01-18 00:38:20 +0300

    Simplify akka-http output writing, get rid of WSSubscriptionStage

--- server/akkahttp/src/main/scala/korolev/akkahttp/AkkaHttpServerConfig.scala
+++ server/akkahttp/src/main/scala/korolev/akkahttp/AkkaHttpServerConfig.scala
@@ -1,7 +1,9 @@
 package korolev.akkahttp
 
-case class AkkaHttpServerConfig(maxRequestBodySize: Int = AkkaHttpServerConfig.DefaultMaxRequestBodySize)
+case class AkkaHttpServerConfig(maxRequestBodySize: Int = AkkaHttpServerConfig.DefaultMaxRequestBodySize,
+                                outputBufferSize: Int = AkkaHttpServerConfig.DefaultOutputBufferSize)
 
 object AkkaHttpServerConfig {
   val DefaultMaxRequestBodySize: Int = 8 * 1024 * 1024
+  val DefaultOutputBufferSize: Int = 1000
 }
