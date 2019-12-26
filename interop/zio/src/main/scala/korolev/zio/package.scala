119693f4871e1055ad48d099040012a394f762e1
Aleksei Fomkin <aleksey.fomkin@gmail.com>
2019-12-26 22:04:14 +0300

    Add ZIO example #300

--- interop/zio/src/main/scala/korolev/zio/package.scala
+++ interop/zio/src/main/scala/korolev/zio/package.scala
@@ -1,7 +1,7 @@
 package korolev
 
 import korolev.effect.Effect
-import _root_.zio.{RIO, Task, Runtime}
+import _root_.zio.{Task, Runtime}
 
 package object zio {
   implicit final def taskEffectInstance[R](implicit runtime: Runtime[R]): Effect[Task] =
