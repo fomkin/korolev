49e9408b5d4453129f123cc61b622ce161e26aa4
Aleksei Fomkin <aleksey.fomkin@gmail.com>
2019-12-27 10:22:42 +0300

    Allow to use user defined error events in ZioEffect #300

--- interop/zio/src/main/scala/korolev/zio/ZioEffect.scala
+++ interop/zio/src/main/scala/korolev/zio/ZioEffect.scala
@@ -1,68 +1,75 @@
 package korolev.zio
 
-import _root_.zio.{Exit, FiberFailure, RIO, Runtime, Task, ZIO}
+import _root_.zio.{Exit, FiberFailure, IO, RIO, Runtime, Task, ZIO}
 import korolev.effect.Effect
 
 import scala.concurrent.{ExecutionContext, Future}
 import scala.util.Try
 
-class ZioEffect[R](rts: Runtime[R]) extends Effect[Task] {
+class ZioEffect[R, E](rts: Runtime[R],
+                      liftError: Throwable => E,
+                      unliftError: E => Throwable) extends Effect[ZIO[R, E, *]] {
 
-  def pure[A](value: A): Task[A] =
+  private val unliftErrorP: PartialFunction[E, Throwable] =
+    { case x => unliftError(x) }
+
+  def pure[A](value: A): ZIO[R, E, A] =
     ZIO.succeed(value)
 
-  def delay[A](value: => A): Task[A] =
-    Task.effect(value)
+  def delay[A](value: => A): ZIO[R, E, A] =
+    IO.effect(value).mapError(liftError)
 
-  def fail[A](e: Throwable): Task[A] =
-    Task.fail(e)
+  def fail[A](e: Throwable): ZIO[R, E, A] =
+    Task.fail(e).mapError(liftError)
 
-  def unit: Task[Unit] =
+  def unit: ZIO[R, E, Unit] =
     ZIO.unit
 
-  def fromTry[A](value: => Try[A]): Task[A] =
-    Task.fromTry(value)
+  def fromTry[A](value: => Try[A]): ZIO[R, E, A] =
+    Task.fromTry(value).mapError(liftError)
 
-  def promise[A](k: (Either[Throwable, A] => Unit) => Unit): Task[A] =
-    RIO.effectAsync(kk => k(kk apply _.fold(ZIO.fail, ZIO.succeed)))
+  def promise[A](k: (Either[Throwable, A] => Unit) => Unit): ZIO[R, E, A] =
+    ZIO.effectAsync(kk => k(kk apply _.fold(liftError.andThen(ZIO.fail), ZIO.succeed)))
 
-  def promiseF[A](k: (Either[Throwable, A] => Unit) => Task[Unit]): Task[A] =
-    RIO.effectAsyncM(kk => k(kk apply _.fold(ZIO.fail, ZIO.succeed)).orDie)
+  def promiseF[A](k: (Either[Throwable, A] => Unit) => ZIO[R, E, Unit]): ZIO[R, E, A] = {
+    ZIO.effectAsyncM(kk => k(kk apply _.fold(liftError.andThen(ZIO.fail), ZIO.succeed)))
+  }
 
-  def flatMap[A, B](m: Task[A])(f: A => Task[B]): Task[B] =
+  def flatMap[A, B](m: ZIO[R, E, A])(f: A => ZIO[R, E, B]): ZIO[R, E, B] =
     m.flatMap(f)
 
-  def map[A, B](m: Task[A])(f: A => B): Task[B] =
+  def map[A, B](m: ZIO[R, E, A])(f: A => B): ZIO[R, E, B] =
     m.map(f)
 
-  def recover[A](m: Task[A])(f: PartialFunction[Throwable, A]): Task[A] =
-    m.catchSome(f.andThen(ZIO.succeed))
+  def recover[A](m: ZIO[R, E, A])(f: PartialFunction[Throwable, A]): ZIO[R, E, A] = {
+    m.catchSome(unliftErrorP.andThen(f).andThen(ZIO.succeed))
+  }
 
-  def start[A](task: => Task[A])(implicit ec: ExecutionContext): Task[Effect.Fiber[Task, A]] =
+  def start[A](task: => ZIO[R, E, A])(implicit ec: ExecutionContext): ZIO[R, E, Effect.Fiber[ZIO[R, E, *], A]] =
     RIO
-      .interruptible(task)
+      .interruptible(task.mapError(unliftError))
       .nonDaemon
       .fork
       .daemon
       .map { fiber =>
-        () => fiber.join
+        () => fiber.join.mapError(liftError)
       }
 
-  def fork[A](m: => Task[A])(implicit ec: ExecutionContext): Task[A] =
+  def fork[A](m: => ZIO[R, E, A])(implicit ec: ExecutionContext): ZIO[R, E, A] =
     m.on(ec)
 
-  def sequence[A](in: List[Task[A]]): Task[List[A]] =
+  def sequence[A](in: List[ZIO[R, E, A]]): ZIO[R, E, List[A]] =
     ZIO.sequence(in)
 
-  def runAsync[A](m: Task[A])(callback: Either[Throwable, A] => Unit): Unit =
+  def runAsync[A](m: ZIO[R, E, A])(callback: Either[Throwable, A] => Unit): Unit =
     rts.unsafeRunAsync(m)(exit => callback(exit.toEither))
 
-  def run[A](m: Task[A]): A =
+  def run[A](m: ZIO[R, E, A]): A =
     rts.unsafeRunSync(m) match {
       case Exit.Success(value) => value
       case Exit.Failure(cause) => throw FiberFailure(cause)
     }
 
-  def toFuture[A](m: Task[A]): Future[A] =
-    rts.unsafeRunToFuture(m)
+  def toFuture[A](m: ZIO[R, E, A]): Future[A] =
+    rts.unsafeRunToFuture(m.mapError(unliftError))
 }
--- interop/zio/src/main/scala/korolev/zio/package.scala
+++ interop/zio/src/main/scala/korolev/zio/package.scala
@@ -1,9 +1,26 @@
 package korolev
 
 import korolev.effect.Effect
-import _root_.zio.{Task, Runtime}
+import _root_.zio.{Runtime, Task, ZIO}
 
 package object zio {
-  implicit final def taskEffectInstance[R](implicit runtime: Runtime[R]): Effect[Task] =
-    new ZioEffect[R](runtime)
+
+  /**
+    * Provides [[Effect]] instance for ZIO[Any, Throwable, *].
+    * Use this method if your app uses [[Throwable]] to express errors.
+    */
+  def taskEffectInstance[R](runtime: Runtime[R]): Effect[Task] =
+    new ZioEffect[Any, Throwable](runtime, identity, identity)
+
+  /**
+    * Provides [[Effect]] instance for ZIO with arbitrary runtime
+    * and error types. Korolev uses Throwable inside itself.
+    * That means if you want to work with your own [[E]],
+    * you should provide functions to convert [[Throwable]]
+    * to [[E]] and vice versa.
+    */
+  final def zioEffectInstance[R, E](runtime: Runtime[R])
+                                   (liftError: Throwable => E)
+                                   (unliftError: E => Throwable): Effect[ZIO[R, E, *]] =
+    new ZioEffect[R, E](runtime, liftError, unliftError)
 }
