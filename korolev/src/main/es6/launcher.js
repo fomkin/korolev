f0b968accd9c0fa02eeca734d2ce817f3d80b8b6
Aleksey Fomkin <aleksey.fomkin@gmail.com>
2020-08-17 16:05:20 +0300

    Add 'KorolevReady' event and 'ready' propery to 'Korolev' global object

--- korolev/src/main/es6/launcher.js
+++ korolev/src/main/es6/launcher.js
@@ -2,10 +2,15 @@ import { Connection } from './connection.js';
 import { Bridge, setProtocolDebugEnabled } from './bridge.js';
 import { ConnectionLostWidget, getDeviceId } from './utils.js';
 
+function showKorolevIsNotReadyMessage() {
+  console.log("Korolev is not ready");
+}
+
 window['Korolev'] = {
   'setProtocolDebugEnabled': setProtocolDebugEnabled,
-  'invokeCallback': () => console.log("Korolev is not ready"),
-  'swapElementInRegistry': () => console.log("Korolev is not ready")
+  'invokeCallback': () => showKorolevIsNotReadyMessage(),
+  'swapElementInRegistry': () => showKorolevIsNotReadyMessage(),
+  'ready': false
 };
 
 window.document.addEventListener("DOMContentLoaded", () => {
@@ -28,12 +33,17 @@ window.document.addEventListener("DOMContentLoaded", () => {
   window['Korolev']['connect'] = () => connection.connect();
 
   connection.dispatcher.addEventListener('open', () => {
+
     clw.hide();
     let bridge = new Bridge(config, connection);
+    let globalObject = window['Korolev']
 
-    window['Korolev']['swapElementInRegistry'] = (a, b) => bridge._korolev.swapElementInRegistry(a, b);
-    window['Korolev']['element'] = (id) => bridge._korolev.element(id);
-    window['Korolev']['invokeCallback'] = (name, arg) => bridge._korolev.invokeCustomCallback(name, arg);
+    globalObject['swapElementInRegistry'] = (a, b) => bridge._korolev.swapElementInRegistry(a, b);
+    globalObject['element'] = (id) => bridge._korolev.element(id);
+    globalObject['invokeCallback'] = (name, arg) => bridge._korolev.invokeCustomCallback(name, arg);
+    globalObject['ready'] = true;
+
+    window.dispatchEvent(new Event('KorolevReady'));
 
     let closeHandler = (event) => {
       bridge.destroy();
